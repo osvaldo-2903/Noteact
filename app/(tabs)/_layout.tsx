42d046f9d2807a3a92e5ee7e2dc1ff1cfc3ac2a7
Brian A. Valles Vela <briansitovalles@gmail.com>
2025-02-10 19:47:51 -0600

    Se estilizo la barra de navegacion y el header para que queden con un color oscuro

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,7 +5,15 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: "#ffd33d"
+        tabBarActiveTintColor: "#ffd33d",
+        headerStyle: {
+          backgroundColor: "#25292e",
+        },
+        headerShadowVisible: false,
+        headerTintColor: "#fff",
+        tabBarStyle: {
+          backgroundColor: "#25292e"
+        }
       }}
     >
       <Tabs.Screen 
