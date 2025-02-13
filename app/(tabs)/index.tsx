389d0563b70d0be498fbf4de39f12d07472ef6e7
Brian A. Valles Vela <briansitovalles@gmail.com>
2025-02-12 18:42:29 -0600

    Se agrego una libreria para procesar imagenes, y se esta modificando el index para colocar imagenes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,12 @@
 import { Text, View, StyleSheet } from "react-native";
-import { Link } from "expo-router";
+import ImageViewer from "../components/ImageViewer";
+
+const 
 
 export default function Index() {
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Home screen</Text>
-
-            <Link href="/about" style={styles.button}>
-                Go to About screen
-            </Link>
+        <View style={styles.imageContainer}>
+            <Image source={Image} style={styles.image} />
         </View>
     );
 }
@@ -22,10 +20,18 @@ const styles = StyleSheet.create({
     },
     text: {
         color: "#fff"
-    }, 
+    },
     button: {
         fontSize: 20,
         textDecorationLine: "underline",
         color: "#fff"
+    },
+    imageContainer: {
+        flex: 1,
+    },
+    image: {
+        width: 320,
+        height: 440,
+        borderRadius: 18,
     }
 });
