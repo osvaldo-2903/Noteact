ee80f14a072e1b91bc99c32125e8fd208cdf1895
Brian A. Valles Vela <briansitovalles@gmail.com>
2025-02-19 20:15:56 -0600

    Se inicio un nuevo proyecto y se reinicio lo que se tenia

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,32 +1,12 @@
-import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
+import React from 'react';
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+const NotFound: React.FC = () => {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1>404 - No Encontrado</h1>
+            <p>La página que estás buscando no existe.</p>
+        </div>
+    );
+};
 
-export default function NotFoundScreen() {
-  return (
-    <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
-        </Link>
-      </ThemedView>
-    </>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-});
+export default NotFound;
