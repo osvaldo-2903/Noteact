ec572d8c824afa4d0f90035c3ff093dd87aa0d53
Brian A. Valles Vela <briansitovalles@gmail.com>
2025-02-19 20:37:08 -0600

    Se agrego la funcionalidad de login

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,22 @@
-import { Stack } from "expo-router";
+import { Slot, useRouter } from "expo-router";
+import { useEffect } from "react";
+import { auth } from "@/firebaseConfig";
+import { onAuthStateChanged } from "firebase/auth";
 
 export default function RootLayout() {
-  return <Stack />;
+  const router = useRouter();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        router.replace("/(tabs)/home"); // Usuario logueado, redirige a Home
+      } else {
+        router.replace("/(auth)/login"); // Usuario no logueado, redirige a Login
+      }
+    });
+
+    return unsubscribe; // Limpia el listener al desmontar
+  }, []);
+
+  return <Slot />;
 }
